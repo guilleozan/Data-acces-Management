9fd0dc152d8317dc4ef6fc37c983720b9810b4d9
grace-morganmaxwell <gracemorganmaxwell@gmail.com>
2024-06-20 07:26:41 +0000

    updated models and migrated

--- School/subjects/forms.py
+++ School/subjects/forms.py
@@ -4,4 +4,8 @@ from .models import Article
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = ['title', 'content', 'category']
+        fields = [
+            'title', 'category', 'type', 'born', 'died', 'nationality', 'known_for',
+            'notable_work', 'about', 'year', 'medium', 'dimensions', 'location',
+            'designed_by', 'developer'
+        ]
