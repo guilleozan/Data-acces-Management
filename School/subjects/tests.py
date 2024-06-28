0a6081ccac7e40f3605c61bb40d7b2f06cd76983
Declan Watts <declan@wattz.dev>
2024-06-28 15:16:44 +1200

    Fixes

--- School/subjects/tests.py
+++ School/subjects/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Category, Article
+from .models import Category, Article, Keyword, ArticleKeyword
 
 class CategoryModelTest(TestCase):
     def setUp(self):
@@ -17,3 +17,23 @@ class ArticleModelTest(TestCase):
     def test_article_title(self):
         article = Article.objects.get(title="Test Article")
         self.assertEqual(article.title, "Test Article")
+
+class KeywordModelTest(TestCase):
+    def setUp(self):
+        Keyword.objects.create(keyword="Impressionism")
+
+    def test_keyword_creation(self):
+        keyword = Keyword.objects.get(keyword="Impressionism")
+        self.assertEqual(keyword.keyword, "Impressionism")
+
+class ArticleKeywordModelTest(TestCase):
+    def setUp(self):
+        category = Category.objects.create(name="Arts")
+        article = Article.objects.create(category=category, title="Mona Lisa", type="Painting")
+        keyword = Keyword.objects.create(keyword="Renaissance")
+        ArticleKeyword.objects.create(article=article, keyword=keyword)
+
+    def test_article_keyword_creation(self):
+        article_keyword = ArticleKeyword.objects.get(article__title="Mona Lisa", keyword__keyword="Renaissance")
+        self.assertEqual(article_keyword.article.title, "Mona Lisa")
+        self.assertEqual(article_keyword.keyword.keyword, "Renaissance")
